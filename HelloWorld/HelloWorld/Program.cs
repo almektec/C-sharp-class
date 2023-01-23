8e45c0f8505f8fb971c52da6f08bdf68dd686580
almektec <superpearsauce@gmail.com>
2023-01-22 19:07:02 -0600

    Hello World project for C sharp class

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿// Topaz is a lovely dog
+
+System.Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Topaz is a dog");
+Console.WriteLine("Topaz is a cute dog /* absolutely */");
+
+string name;
+name = " Topaz"; // test comment
+Console.WriteLine("Hi" + name);
+
+string name2;
+Console.WriteLine("What is your dog's name?");
+name2 = Console.ReadLine();
+Console.WriteLine("Hi" + name2);
+
+/* multi
+ * line
+ * comment
+ * */
