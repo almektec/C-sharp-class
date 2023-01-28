78b86318e6db1bd608bca91d36f9d51b2b829bbb
almektec <superpearsauce@gmail.com>
2023-01-27 20:24:42 -0600

    TheVariableShop Class Project

---
+++ TheVariableShop/TheVariableShop/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 1;
+double b = 1.13498239842314;
+decimal c = 11;
+string d = "hi";
+float e = 32;
+byte f = 10;
+short g = 3;
+long h = 1;
+sbyte i = 1;
+ushort j = 1;
+uint k = 1;
+ulong l = 1;
+
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+Console.WriteLine(l);
