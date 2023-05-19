2fddb928be49ba53d99bbe331810aa7fcc579678
almektec <superpearsauce@gmail.com>
2023-05-19 10:09:09 -0500

    Buying inventory project

---
+++ RepairingTheClocktower/RepairingTheClocktower/Program.cs
@@ -0,0 +1,9 @@
+﻿// take a number as input from the console
+
+Console.Write("pick a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// display the word tick if the number is even, dispaly the word tock if the number is odd
+
+if (number % 2 == 0) Console.WriteLine("Tick");
+else Console.WriteLine("Tock");
