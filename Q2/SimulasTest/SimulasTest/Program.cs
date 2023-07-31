91ce3c85f99652205d79017eacdfb097c8f7e9e7
almektec <superpearsauce@gmail.com>
2023-07-31 16:09:51 -0500

    Move Q2 projects to Q2 folder.

---
+++ Q2/SimulasTest/SimulasTest/Program.cs
@@ -0,0 +1,19 @@
+﻿//define an enumeration for the state of the chest
+//make a variable whose type is this new enumeration
+ChestState currentState = ChestState.Locked;
+
+while (true)
+{
+    Console.Write($"The chest is {currentState}. What do you want to do? ");
+
+    string input = Console.ReadLine();
+    //write code to allow you to manipulate the chest with the lock, unlock, open, and close cmds
+    if (currentState == ChestState.Locked && input == "unlock") currentState = ChestState.Closed;
+    if (currentState == ChestState.Closed && input == "open") currentState = ChestState.Open;
+    if (currentState == ChestState.Open && input == "close") currentState = ChestState.Closed;
+    if (currentState == ChestState.Closed && input == "lock") currentState = ChestState.Locked;
+}
+
+enum ChestState { Open, Closed, Locked }
+
+//loop forever
